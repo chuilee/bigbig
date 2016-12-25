2ae57a8221b065b12fc25ab8662eef6dea3a318a
chuilee <chui.lee8@gmail.com>
2016-12-25 20:58:47 +0800

    首页修改

--- routes/json/gallery.js
+++ routes/json/gallery.js
@@ -4,7 +4,7 @@ var Post = keystone.list('Post');
 exports = module.exports = function (req, res) {
 	Post.model.find({state: 'published'})
 		.sort('sortOrder')
-		.select({'image.url': 1, 'image.secure_url': 1})
+		.select({'image.url': 1, 'image.secure_url': 1, name: 1})
 		.exec(function(err, result){
 			res.setHeader('Content-Type', 'application/json');
 			res.send(JSON.stringify(result));
